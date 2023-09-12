75f28b3f89d3d97a6dbd9e5691bed3893af3e3bf
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-12 10:05:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-// Array of API URLs to fetch data from
+// Array of API URLs
 const apiUrls = [
-  "https://jsonplaceholder.typicode.com/todos/1",
-  "https://jsonplaceholder.typicode.com/todos/2",
-  "https://jsonplaceholder.typicode.com/todos/3",
-  "https://jsonplaceholder.typicode.com/todos/4",
-  "https://jsonplaceholder.typicode.com/todos/5",
-  "https://jsonplaceholder.typicode.com/todos/6",
-  "https://jsonplaceholder.typicode.com/todos/7",
-  "https://jsonplaceholder.typicode.com/todos/8",
-  "https://jsonplaceholder.typicode.com/todos/9",
-  "https://jsonplaceholder.typicode.com/todos/10",
+    'https://api.example.com/1',
+    'https://api.example.com/2',
+    // Add more API URLs here
 ];
 
-// You can write your code here
+// Function to fetch data from a single API
+function fetchData(apiUrl) {
+    return fetch(apiUrl).then(response => response.json());
+}
+
+// Function to measure the time taken for a Promise to resolve
+function measureTime(promise) {
+    const startTime = performance.now();
+    return promise.then(() => performance.now() - startTime);
+}
+
+// Function to compare Promise.all and Promise.any
+async function comparePerformance() {
+    const allPromises = apiUrls.map(apiUrl => measureTime(fetchData(apiUrl)));
+    const anyPromise = Promise.any(allPromises);
+
+    const [allTime, anyTime] = await Promise.all([Promise.all(allPromises), anyPromise]);
+
+    // Display the results on the webpage
+    document.getElementById('output-all').textContent = allTime.reduce((acc, time) => acc + time, 0);
+    document.getElementById('output-any').textContent = anyTime;
+
+    // Note: 'allTime' is an array of times taken for each Promise in Promise.all
+}
+
+// Call the comparison function
+comparePerformance();
